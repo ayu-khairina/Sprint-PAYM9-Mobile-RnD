e54c8daf65d032689d8dea631c5c8fc42de4884a
tyo-sinbad <prasetyo@sinbad.co.id>
2020-10-05 18:02:37 +0700

    add textInput

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -1,13 +1,22 @@
 import React, {useState, useEffect} from 'react';
-import { Text, View, PermissionsAndroid, TouchableOpacity } from 'react-native';
+import { Text, View, PermissionsAndroid, TouchableOpacity, TextInput } from 'react-native';
 import Contacts from 'react-native-contacts';
+import Modal from 'react-native-modal';
 
 function addContact() {
-  const [permission, setPermission] = useState(null)
+  const [modalContact, setModalContact] = useState(false)
+  const [searchContact, setSearchContact] = useState(null);
+  const [modalAddPelanggan, setModalAddPelanggan] = useState(false)
+  const [nama, setNama] = useState(null)
+  const [handphone, setHandphone] = useState(null)
+  const [note, setNote] = useState(null)
+  const [list, setList] = useState([{nama: null, handphone:null, note: null}])
 
-const contactPermission = () => {
-  PermissionsAndroid.request(
-    PermissionsAndroid.PERMISSIONS.READ_CONTACTS,PermissionsAndroid.PERMISSIONS.WRITE_CONTACTS)
+const contactPermission = async () => {
+  await PermissionsAndroid.requestMultiple([
+      PermissionsAndroid.PERMISSIONS.READ_CONTACTS,
+      PermissionsAndroid.PERMISSIONS.WRITE_CONTACTS
+    ])
     .then((res) => {
       add_contact()
     })
@@ -32,31 +41,162 @@ const add_contact = () => {
   Contacts.addContact(newPerson, (err) => {
     if (err) {
       console.log("ini error:", err);
+    } else {
+        setModalContact(true)
+      // Contacts.getAll((err, contacts) => {
+      //   if (err === 'denied'){
+      //     // error
+      //   } else {
+      //     console.log("dataa:", contacts);
+      //     // contacts returned in Array
+      //   }
+      // })
     }
-  })
-  // Contacts.getAll((err, contacts) => {
-  //   if (err === 'denied'){
-  //     // error
-  //   } else {
-  //     console.log("dataa:", contacts);
-  //     // contacts returned in Array
-  //   }
-  // })
+  })  
+}
+
+const openModalContact = () => {
+  return (
+    <View style={{flex: 1}}>
+      <Modal 
+        isVisible={modalContact} 
+        style={{backgroundColor:"white", flex: 1, margin:0}} 
+        animationIn="slideInUp" 
+        animationOut="slideOutDown" 
+        coverScreen={true}
+        useNativeDriver={true}
+      >
+        <View style={{flex: 1}}>
+          <View style={{flexDirection:"row", margin:10}}>
+            <View >
+              <TouchableOpacity onPress={()=> setModalContact(false)} style={{margin:0, backgroundColor:"red", height: 40}}>
+                <Text style={{fontSize: 16, alignSelf:"center", color:"white", margin:5, marginHorizontal:16}}>{`<--`}</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={{flex: 2}}>
+              <TextInput
+                style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+                onChangeText={text => setSearchContact(text)}
+                value={searchContact}
+              />
+            </View>
+            <View>
+              <TouchableOpacity onPress={()=> alert("search")} style={{margin:0, backgroundColor:"red", height: 40}}>
+                <Text style={{fontSize: 16, alignSelf:"center", color:"white", margin:5}}>{`Search`}</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View style={{alignItems:"center"}} > 
+            <TouchableOpacity style={{backgroundColor:"green"}} onPress={()=>setModalAddPelanggan(true) } >
+              <View style={{flexDirection:"row", margin: 10}}>
+                <Text style={{color:"white"}}>+ </Text>
+                <Text style={{color:"white"}}>Tambah Pelanggan Baru</Text>
+              </View>
+            </TouchableOpacity>  
+          </View>
+        </View>
+
+      </Modal>
+    </View>
+  )
+}
+
+const openModalAddPelanggan = () => {
+  return (
+    <View style={{flex: 1}}>
+      <Modal 
+        isVisible={modalAddPelanggan} 
+        style={{backgroundColor:"white", flex: 1, margin:0}} 
+        animationIn="slideInUp" 
+        animationOut="slideOutDown" 
+        coverScreen={true}
+        useNativeDriver={true}
+      >
+        <View style={{flex: 1}}>
+          <View>
+            <View style={{flexDirection:"row", alignSelf:"space-between", margin:10}}>
+              <Text style={{marginRight: 10}}>Nama</Text>
+              <TextInput
+              style={{ height: 40, borderColor: 'gray', borderWidth: 1, width:"80%" }}
+              onChangeText={text => setNama(text)}
+              value={nama} />
+            </View>
+            <View style={{flexDirection:"row", alignSelf:"space-between", margin:10}}>
+              <Text style={{marginRight: 10}}>Handphone</Text>
+              <TextInput
+              style={{ height: 40, borderColor: 'gray', borderWidth: 1, width:"80%" }}
+              onChangeText={text => setHandphone(text)}
+              value={handphone} />
+            </View>
+            <View style={{flexDirection:"row", alignSelf:"space-between", margin:10}}>
+              <Text style={{marginRight: 10}}>Note</Text>
+              <TextInput
+              style={{ height: 40, borderColor: 'gray', borderWidth: 1, width:"80%" }}
+              onChangeText={text => setNote(text)}
+              value={note} />
+            </View>
+            <View style={{flexDirection:"row", alignSelf:"space-between", margin:10}} >
+              <TouchableOpacity onPress={()=> setModalAddPelanggan(false)} style={{backgroundColor:"red", borderRadius:10, marginRight: 16}}>
+                <Text style={{color: "white", margin:16}}>Cancel</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={()=> savePelanggan()} style={{backgroundColor:"green", borderRadius:10, marginRight: 16}}>
+                <Text style={{color: "white", margin:16}}>Save</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </Modal>
+    </View>
+  )
+}
+
+const savePelanggan = () => {
+  setList([{nama: nama, handphone: handphone, note: note}])
+  setModalAddPelanggan(false)
+  setModalContact(false)
+  alert("success")
 }
 
 const button_add_contact = () => {
   return (
-    <View>
-      <TouchableOpacity onPress={() => contactPermission()}>
-        <Text>add contact</Text>
+    <View style={{margin: 10, flex: 1}}>
+      <TouchableOpacity onPress={() => contactPermission()} style={{backgroundColor:"green"}}>
+        <Text style={{fontSize: 16, alignSelf:"center", color:"white", margin:16}}> + add contact</Text>
       </TouchableOpacity>
     </View>
   );
 }
 
+const listHutang = () => {
+  return (
+    <View style={{margin: 10}}>
+      {
+      nama === null && handphone === null && note === null?
+        <Text>Data kosong</Text>
+      :
+        <View>
+          <Text>{nama}</Text>
+          <Text>{note}</Text>
+        </View>
+      }
+    </View>
+  );
+}
+
+const renderMain = () => {
   return(
-    <View>
-      {button_add_contact()}
+  <View style={{flex: 1}}>
+    {button_add_contact()}
+    {listHutang()}
+  </View>
+  )
+}
+
+  return(
+    <View style={{flex: 1}}>
+      {renderMain()}
+      {openModalContact()}
+      {openModalAddPelanggan()}
     </View>
   )
 

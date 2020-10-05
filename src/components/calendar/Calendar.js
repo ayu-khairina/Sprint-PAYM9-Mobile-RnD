3d83bcc1a2352843fea13d3b4a7b8833aa3f233e
tyo-sinbad <prasetyo@sinbad.co.id>
2020-10-05 13:31:17 +0700

    add contact

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -1,10 +1,65 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { Text, View, PermissionsAndroid, TouchableOpacity } from 'react-native';
+import Contacts from 'react-native-contacts';
 
-const Calendar = () => {
+function addContact() {
+  const [permission, setPermission] = useState(null)
+
+const contactPermission = () => {
+  PermissionsAndroid.request(
+    PermissionsAndroid.PERMISSIONS.READ_CONTACTS,PermissionsAndroid.PERMISSIONS.WRITE_CONTACTS)
+    .then((res) => {
+      add_contact()
+    })
+    .catch((err)=> {
+      alert(err)
+    });
+}
+
+const add_contact = () => {
+  const newPerson = {
+    emailAddresses: [{
+      label: "work",
+      email: "mrniet@example.com",
+    }],
+    familyName: "Nietzsche",
+    givenName: "Friedrich",
+    phoneNumbers: [{
+      label: "mobile",
+      number: "0822098732883"
+    }]
+  }
+  Contacts.addContact(newPerson, (err) => {
+    if (err) {
+      console.log("ini error:", err);
+    }
+  })
+  // Contacts.getAll((err, contacts) => {
+  //   if (err === 'denied'){
+  //     // error
+  //   } else {
+  //     console.log("dataa:", contacts);
+  //     // contacts returned in Array
+  //   }
+  // })
+}
+
+const button_add_contact = () => {
   return (
-    <Text>Hello, this is Calendar!</Text>
+    <View>
+      <TouchableOpacity onPress={() => contactPermission()}>
+        <Text>add contact</Text>
+      </TouchableOpacity>
+    </View>
   );
 }
 
-export default Calendar;
+  return(
+    <View>
+      {button_add_contact()}
+    </View>
+  )
+
+}
+
+export default addContact;

82cbecfde8b381bc5aa0751630d145f91f5a6ae1
wahyukhairina <ayu@sinbad.co.id>
2020-10-05 10:34:35 +0700

    add bottom navigator

---
+++ src/components/calendar/Calendar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const Calendar = () => {
+  return (
+    <Text>Hello, this is Calendar!</Text>
+  );
+}
+
+export default Calendar;
---
+++ src/components/contact/Contact.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const Contact = () => {
+  return (
+    <Text>Hello, this is Contact!</Text>
+  );
+}
+
+export default Contact;

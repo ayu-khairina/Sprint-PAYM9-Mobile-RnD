3db6e35b761de760e4a39c289cf384e19a905733
wahyukhairina <ayu@sinbad.co.id>
2020-10-05 13:29:51 +0700

    add react-native-contact

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,10 +1,42 @@
-import React from 'react';
-import { Text } from 'react-native';
-
-const Contact = () => {
+import React, {useState, useEffect} from 'react';
+import { Text, View, PermissionsAndroid, TouchableOpacity } from 'react-native';
+import Contacts from 'react-native-contacts';
+function Contact() {
+  const [permission, setPermission] = useState(null)
+const contactPermission = () => {
+  PermissionsAndroid.request(
+    PermissionsAndroid.PERMISSIONS.READ_CONTACTS,
+    {
+      'title': 'Contacts',
+      'message': 'This app would like to view your contacts.',
+      'buttonPositive': 'Please accept bare mortal'
+    }
+  ).then(() => {
+    Contacts.getAll((err, contacts) => {
+      if (err === 'denied'){
+        // error
+      } else {
+        // contacts returned in Array
+      }
+    })
+  });
+}
+const test = () => {
+  alert("test")
+}
+const add_contact = () => {
   return (
-    <Text>Hello, this is Contact!</Text>
+    <View>
+      <TouchableOpacity onPress={() => contactPermission()}>
+        <Text>add contact</Text>
+      </TouchableOpacity>
+    </View>
   );
 }
-
+  return(
+    <View>
+      {add_contact()}
+    </View>
+  )
+}
 export default Contact;

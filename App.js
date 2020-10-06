ee76424aabcfee86bd77545aab6a1d0abf21b7a4
tyo-sinbad <prasetyo@sinbad.co.id>
2020-10-06 18:16:27 +0700

    add calendar

--- App.js
+++ App.js
@@ -4,11 +4,13 @@ import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import Calendar from './src/components/calendar/Calendar'
 import Contact from './src/components/contact/Contact'
+import KartuNama from './src/components/KartuNama/kartu_nama'
 
 
 const TabNavigator = createBottomTabNavigator({
   Contact: Contact,
   Calendar: Calendar,
+  KartuNama: KartuNama,
 });
 
 export default createAppContainer(
